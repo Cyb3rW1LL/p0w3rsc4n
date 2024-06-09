b57198ee3c3a2f336672def5fbc8eceeef01d897
Cyb3rW1LL <39623516+Cyb3rW1LL@users.noreply.github.com>
2024-06-09 11:07:46 -0500

    Update p0w3r$c4n.ps1

--- p0w3r$c4n.ps1
+++ p0w3r$c4n.ps1
@@ -7,6 +7,27 @@
 # Install-Module PSParallel
 Import-Module PSParallel
 
+# Function to send a TCP SYN packet and get the TTL value
+function GET-TTL {
+    param (
+        [string]$Target,
+        [int]$Ports = 443
+    )
+    try {
+        # Send a TCP SYN packet to the target
+        $tcpClient = New-Object System.Net.Sockets.TcpClient
+        $tcpClient.Connect($Target, $Port)
+        $tcpClient.Close()
+        
+        # Get the TTL value
+        $ttl = $tcpClient.Client.Ttl
+        return $ttl
+    } catch {
+        Write-Host "Failed to connect to $Target on port $Port"
+        return $null
+    }
+}
+
 # Standard Tcptarget scan
 # Will either connect or potentially be filtered
 function TCP-SCAN {
