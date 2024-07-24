883688f066816486e82a514c90ebb0bc07a74248
Cyb3rW1LL <39623516+Cyb3rW1LL@users.noreply.github.com>
2024-07-24 08:21:19 -0500

    Update p0w3r$c4n.ps1

--- p0w3r$c4n.ps1
+++ p0w3r$c4n.ps1
@@ -1,9 +1,15 @@
 # P0w3r$c4n.ps1
 # Author: Cyb3rW1LL
- 
+
+
+# We will not use Test-Connecction as we do not want to alert on anomalous ICMP traffic
+# Instead, we will leverage the New-Object System.Net.Sockets library to gather TCP, UDP,
+# And TTL information about the target host(s)
+
+
 
 # Commandline parameters to run scan with
-# EXAMPLE \.scanner.ps1 -ComputerName localhost 
+# EXAMPLE \.scanner.ps1 -ComputerName localhost -p 80 -t -o "D:\test\localhost.csv"
 
 $def_scan1 = @(1..1000)
 $scan_1 = @(20,21,22,23,25,53,80,88,111,121,123,135,137,138,139,389,443,445,464,5044,5353,5601,8200,9200,9300,9800)
